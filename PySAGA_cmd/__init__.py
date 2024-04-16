e212ce07055f48a7693849a6d97ab692ffae4022
Alex-Andrei Cuvuliuc <cuvuliucalexandrei@gmail.com>
2024-04-16 19:16:07 +0300

    import star

--- PySAGA_cmd/__init__.py
+++ PySAGA_cmd/__init__.py
@@ -1,2 +1,20 @@
-from .saga import SAGA
-from .objects import get_sample_dem
+from .saga import (
+    SAGA,
+    Library,
+    Tool,
+    )
+from .objects import (
+    Raster,
+    Vector,
+    get_sample_dem
+)
+
+
+__all__ = [
+    'SAGA',
+    'Library',
+    'Tool',
+    'Raster',
+    'Vector',
+    'get_sample_dem'
+]
